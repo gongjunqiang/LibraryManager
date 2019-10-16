b4c5e42b4ad8c1827cb6a97bf42f87de4f791a86
GJQ <1208754526@qq.com>
2019-10-16 18:26:00 +0800

    dAPPERyinru

--- Library/DAL/SysAdminService.cs
+++ Library/DAL/SysAdminService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +8,7 @@ using DBUtility;
 using Models;
 using System.Data;
 using System.Data.SqlClient;
+using Dapper;
 
 namespace DAL
 {
@@ -15,6 +17,14 @@ namespace DAL
     /// </summary>
     public class SysAdminService
     {
+        private static string connString = ConfigurationManager.ConnectionStrings["connString"].ToString();
+        public SqlConnection OpenConnection()
+        {
+            SqlConnection conn = new SqlConnection(connString);
+            conn.Open();
+            return conn;
+        }
+
         /// <summary>
         /// 管理员登录
         /// </summary>
@@ -22,33 +32,39 @@ namespace DAL
         /// <returns></returns>
         public SysAdmin AdminLogin(SysAdmin admin)
         {
-            string sql = "select AdminName,StatusId,RoleId from SysAdmins where AdminId=@AdminId and LoginPwd=@LoginPwd";
-            SqlParameter[] sqlParameters = new SqlParameter[]
+            string sql = "select AdminId,AdminName,LoginPwd,StatusId,RoleId from SysAdmins where AdminId=@AdminId and LoginPwd=@LoginPwd";
+            using (var conn = OpenConnection())
             {
-                new SqlParameter("@AdminId",admin.AdminId),
-                new SqlParameter("@LoginPwd",admin.LoginPwd), 
-            };
+                return conn.Query<SysAdmin>(sql, new { AdminId = admin.AdminId, LoginPwd = admin.LoginPwd })
+                    .FirstOrDefault();
+            }   
 
-            try
-            {
-                SqlDataReader reader = SqlHelper.ExecuteReader(sql, sqlParameters);
-                if (reader.Read())
-                {
-                    admin.AdminName = reader["AdminName"].ToString();
-                    admin.RoleId = Convert.ToInt32(reader["RoleId"]);
-                    admin.StatusId = Convert.ToInt32(reader["StatusId"]);
-                }
-                else
-                {
-                    admin = null;
-                }
-                reader.Close();
-                return admin;
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
+            //SqlParameter[] sqlParameters = new SqlParameter[]
+            //{
+            //    new SqlParameter("@AdminId",admin.AdminId),
+            //    new SqlParameter("@LoginPwd",admin.LoginPwd), 
+            //};
+
+            //try
+            //{
+            //    SqlDataReader reader = SqlHelper.ExecuteReader(sql, sqlParameters);
+            //    if (reader.Read())
+            //    {
+            //        admin.AdminName = reader["AdminName"].ToString();
+            //        admin.RoleId = Convert.ToInt32(reader["RoleId"]);
+            //        admin.StatusId = Convert.ToInt32(reader["StatusId"]);
+            //    }
+            //    else
+            //    {
+            //        admin = null;
+            //    }
+            //    reader.Close();
+            //    return admin;
+            //}
+            //catch (Exception ex)
+            //{
+            //    throw ex;
+            //}
         }
 
         /// <summary>
